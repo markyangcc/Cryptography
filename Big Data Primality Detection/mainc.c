6960fa84a1b79b3f48ec34c71e94be4fe3467e32
markyangcc <guangshengyang01@gmail.com>
2020-11-22 11:20:53 +0800

    update comments

--- Big Data Primality Detection/mainc.c
+++ Big Data Primality Detection/mainc.c
@@ -1,32 +1,43 @@
 #include <math.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 #include "miracl.h"
 #include "mirdef.h"
+
 #define K 6  //安全系数
 
-int farmattest(big p);
+bool fermat_primality_test(big p);
 
 int main() {
   FILE* fp;
-  miracl* mip = mirsys(3000, 10);
 
-  big p = mirvar(0);
+  // mip: Miracl Instance Pointer
+  // miracl* mirsys (int nd, mr_small nb)
+  // Initialises the MIRACL system for the current program thread,
+  // nd The number of digits to use for each big/flash variable. If negative, it
+  // is taken as indicating the
+  // size of big/flash numbers in 8-bit bytes
+  // nb The number base docs:5
+  miracl* mip = mirsys(1500, 10);
 
   if ((fp = fopen("data.txt", "r+")) == NULL) {
-    printf("Error, file open failed");
-    exit(0);
+    fprintf(stderr, "fopen() failed in file %s at line # %d", __FILE__,
+            __LINE__);
+    exit(EXIT_FAILURE);
   }
+
+  big p = mirvar(0);
 
   while (!feof(fp)) {
     cinnum(p, fp);
     cotnum(p, stdout);
-    if (farmattest(p) == 1) {
-      printf("p is %6.4f%% prime\n", 100 * (1 - pow(0.5, K)));
+    if (fermat_primality_test(p) == true) {
+      printf("p is %6.4lf%% prime\n", 100 * (1 - pow(0.5, K)));
     }
-    if (farmattest(p) == 0) {
+    if (fermat_primality_test(p) == false) {
       printf("p is a composite\n");
     }
   }
@@ -34,44 +45,39 @@ int main() {
   return 0;
 }
 
-int farmattest(big p) {
-  int i = 0;
-  int j = 0;
-
-  big randnum = mirvar(0);  //生成一个随机数
+bool fermat_primality_test(big p) {
+  big a = mirvar(0);  //生成一个随机数
   big pminus1 = mirvar(0);
   big pminus2 = mirvar(0);
   big mgcd = mirvar(0);       //最大公因数
   big rval = mirvar(0);       //模幂运算返回值
-  big constnum1 = mirvar(1);  //
-  big constnum2 = mirvar(2);
+  big constnum1 = mirvar(1);  //常数1
+  big constnum2 = mirvar(2);  //常数2
 
   decr(p, 2, pminus2);  // pminus2 = p - 2;
   decr(p, 1, pminus1);  // pminus1 = p - 1;
 
-  srand((unsigned int)time(NULL));
-
   // void irand (mr_unsign32 seed)
   // Initialises internal random number system. Long integer types are used
-  // internally to yield a generator with
-  // maximum period.
+  // internally to yield a generator with maximum period.
+  irand((unsigned int)time(NULL));  //重新播种
 
   // void bigrand (big w, big x)
   // x A big random number in the range 0 <= x < w
-  bigrand(pminus2, randnum);  //生成随机数a
+  bigrand(pminus2, a);  //生成随机数a
 
-  for (i = 0; i < K; i++) {
+  int j = 0;  //计数
+  for (int i = 0; i < K; i++) {
+    // int egcd (big x, big y, big z)
     // Calculates the Greatest Common Divisor of two big numbers.
-    // z = gcd(x,y)
-    egcd(randnum, p, mgcd);  //计算randnum与m的最大公因数
-                             // 并赋值给mgcd（互素最大公因数为1）
+    // z = gcd(x,y) docs:1
+    egcd(a, p, mgcd);  //计算a与p的最大公因数
+                       // 并赋值给mgcd（互素最大公因数为1）
 
-    // compare returns: +1 if x > y; 0 if x = y; -1 if x < y
+    // int compare* (big x, big y)
+    // returns: +1 if x > y; 0 if x = y; -1 if x < y
     //判断obj和随机数是否互素，它们的最大公因数如果不是1的话，compare函数将会返回1，不满足条件
     if (mr_compare(mgcd, constnum1) == 0) {
-      // void powmod (big x, big y, big n, big w)
-      // w = xy (mod n)
-
       /* 随机选取整数b,2<=b<=n-2;
       计算rval≡b^(n-1)(mod n);
       如果r!=1,则n是合数;
@@ -86,7 +92,9 @@ int farmattest(big p) {
       如果有很多的a能够使等式成立，那么我们可以说n可能是素数，或者伪素数。
       */
 
-      powmod(randnum, pminus1, p, rval);  //如果r=1，则obj可能是素数
+      // void powmod (big x, big y, big n, big w)
+      // w = xy (mod n)
+      powmod(a, pminus1, p, rval);  //如果rval=1，则p可能是素数
 
       // j是判断因子，如果一个数能够满足在当前的轮数下，满足上述的算法，则j能够计数；如果j不等于轮数，那么这个数就不是素数
       if (mr_compare(rval, constnum1) == 0) {
@@ -94,14 +102,14 @@ int farmattest(big p) {
       }
     }
   }
-
+  // 计算了K轮，每次结果都是素数 返回true,否则返回false
   if (j == K) {
-    return 1;
+    return true;
   } else {
-    return 0;
+    return false;
   }
 
-  mirkill(randnum);
+  mirkill(a);
   mirkill(pminus2);
   mirkill(pminus1);
   mirkill(rval);
