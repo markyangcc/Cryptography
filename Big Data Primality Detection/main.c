4d1cbbd0149c8b089075a8772196b4d5934faa3b
markyangcc <guangshengyang01@gmail.com>
2020-11-22 14:03:21 +0800

    Remove some test files and update comments in src code

--- Big Data Primality Detection/main.c
+++ Big Data Primality Detection/main.c
@@ -1,111 +1,136 @@
 #include <math.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 #include "miracl.h"
 
-#define ROUND 6
+#define K 6  //安全系数
 
-int Fermatjdu_prime(big obj);
+bool fermat_primality_test(big p);
 
-int main(void) {
-  FILE *fp;
-  big obj;
+int main() {
+  FILE* fp;
 
   // mip: Miracl Instance Pointer
-  // mirsys(num_of_digits, num_base) docs:5
-  miracl *mip =
-      mirsys(300000, 16);  //每次使用变量的位数(不能小于所求)，16表示数字进制
+  // miracl* mirsys (int nd, mr_small nb)
+  // Initialises the MIRACL system for the current program thread,
+  //
+  // nd The number of digits to use for each big/flash variable. If negative, it
+  // is taken as indicating the
+  // size of big/flash numbers in 8-bit bytes
+  // nb The number base docs:5
+  miracl* mip = mirsys(500, 10);  // This initialises the MIRACL system to
+                                   // use 1500 decimal digits for each
+                                   // big or flash number
 
-  // the 'printable' number base to be used for input and output(range[2,256]).
-  // docs:7
-  mip->IOBASE = 16;
-
-  // all big variables need to be "mirvar"ed
-  obj = mirvar(0);  //初始化变量obj，obj是输入的需要判断是否为素数的大数
-
-  if ((fp = fopen("datatest.txt", "r+")) == NULL) {
-    printf("Error, file open failed...\n");
+  if ((fp = fopen("data.txt", "r+")) == NULL) {
+    fprintf(stderr, "fopen() failed in file %s at line # %d", __FILE__,
+            __LINE__);
     exit(EXIT_FAILURE);
-  }  //判断文件是否能够正确打开
+  }
 
-  //检测文件结束符
+  big p = mirvar(0);
+
   while (!feof(fp)) {
     // int cinnum (flash x, FILE * filep)
     // x Big/flash number, filep File descriptor(stdin/opened file) docs:5
-    cinnum(obj, fp);  // converts them（how many bytes digits) into big number x
+    cinnum(p, fp);  // converts *filep into big number x
 
+    // int cotnum (flash x, FILE * filep)
     // Outputs a big/flash number to the screen or to a file, using as number
     // base the value currently assigned to docs:5
-    cotnum(obj, stdout);
+    mip->IOBASE = 10;  //设置为10进制输出
+    cotnum(p, stdout);
 
-    if (Fermatjdu_prime(obj))
-      printf("This number has a %6.4f%% probability of being a prime number.\n",
-             100 * (1 - pow(0.5, ROUND)));
-    else
-      printf("This number is 100%% definitely a Composite number! \n");
+    // Output
+    if (fermat_primality_test(p) == true) {
+      printf("p is %6.4lf%% prime\n", 100 * (1 - pow(0.5, K)));
+    }
+    if (fermat_primality_test(p) == false) {
+      printf("p is a composite\n");
+    }
   }
-
   fclose(fp);
 
-  // Securely kills off a big/flash number by zeroising it, and freeing its
-  // memory.
-  mirkill(obj);
-
-  // Cleans up after the current instance of MIRACL, and frees all internal
-  // variables.
-  mirexit();
+  mirkill(p);
 
   return 0;
 }
 
-int Fermatjdu_prime(big obj) {
-  big radn, trans, mgcd, trans1, r, num1, num2, cons;
+bool fermat_primality_test(big p) {
+  big a = mirvar(0);  //生成一个随机数
+  big pminus1 = mirvar(0);
+  big pminus2 = mirvar(0);
+  big mgcd = mirvar(0);       //最大公因数
+  big rval = mirvar(0);       //模幂运算返回值
+  big constnum1 = mirvar(1);  //常数1
+  big constnum2 = mirvar(2);  //常数2
 
-  int i, j;
-  miracl *mip = mirsys(1500, 16);
-  mip->IOBASE = 16;
+  decr(p, 2, pminus2);  // pminus2 = p - 2;
+  decr(p, 1, pminus1);  // pminus1 = p - 1;
 
-  radn = mirvar(0);  //对函数中使用到的big型变量进行初始化
-  trans = mirvar(0);
-  mgcd = mirvar(0);
-  trans1 = mirvar(0);
-  r = mirvar(0);
-  num1 = mirvar(1);
-  num2 = mirvar(2);
-  cons = mirvar(0);
+  // void irand (mr_unsign32 seed)
+  // Initialises internal random number system. Long integer types are used
+  // internally to yield a generator with maximum period.
+  irand((unsigned int)time(NULL));  //重新播种
 
-  i = 0;
-  j = 0;
+  // void bigrand (big w, big x)
+  // x A big random number in the range 0 <= x < w
+  bigrand(pminus2, a);  //生成随机数a
 
-  decr(obj, 2, trans);   // trans=obj-2
-  decr(obj, 1, trans1);  // trans=obj-1
-  srand((unsigned int)time(NULL));
-  for (i = 0; i < ROUND; i++) {
-    bigrand(obj, radn);     //生成所需要的随机数
-    egcd(radn, obj, mgcd);  //计算obj和生成的随机数的最大公因数
-    
-    if (!mr_compare(
-            mgcd,
-            num1))  //判断obj和随机数是否互素，它们的最大公因数如果不是1的话，compare函数将会返回1，不满足条件
-    {
-      powmod(radn, trans1, obj,
-             r);  //计算 ，如果r=1，则obj可能是素数，进入下一个if语句
-      if (!mr_compare(r, num1))
-        j++  // j是判断因子，如果一个数能够满足在当前的轮数下，满足上述的算法，则j能够计数；如果j不等于轮数，那么这个数就不是素数
-            ;
+  int j = 0;  //计数
+  for (int i = 0; i < K; i++) {
+    // int egcd (big x, big y, big z)
+    // Calculates the Greatest Common Divisor of two big numbers.
+    // z = gcd(x,y) docs:1
+    egcd(a, p, mgcd);  //计算a与p的最大公因数
+                       // 并赋值给mgcd（若互素，则最大公因数为1）
+
+    // int compare* (big x, big y)
+    // returns: +1 if x > y; 0 if x = y; -1 if x < y
+    //判断obj和随机数是否互素，它们的最大公因数如果不是1的话，compare函数将会返回1，不满足条件
+    if (mr_compare(mgcd, constnum1) == 0) {
+      /* 随机选取整数b,2<=b<=n-2;
+      计算rval≡b^(n-1)(mod n);
+      如果r!=1,则n是合数;
+      上述过程重复t次.
+
+      费马素性检测
+      如果p是素数， 1 ≤ a ≤ p − 1
+      那么
+      a^(p − 1) ≡ 1 ( mod p )
+      如果我们想知道n是否是素数，我们在中间选取a，看看上面等式是否成立。
+      如果对于数值a等式不成立，那么n是合数。
+      如果有很多的a能够使等式成立，那么我们可以说n可能是素数，或者伪素数。
+      */
+
+      // void powmod (big x, big y, big n, big w)
+      // w = xy (mod n)
+      powmod(a, pminus1, p, rval);  //如果rval=1，则p可能是素数
+
+      // j是判断因子，如果一个数能够满足在当前的轮数下，满足上述的算法，则j能够计数；如果j不等于轮数，那么这个数就不是素数
+      if (mr_compare(rval, constnum1) == 0) {
+        j++;
+      }
     }
   }
-  if (j == ROUND)
-    return 1;
-  else
-    return 0;
+  // 计算了K轮，每次结果都是素数 返回true,否则返回false
+  if (j == K) {
+    return true;
+  } else {
+    return false;
+  }
 
-  mirkill(obj);
-  mirkill(radn);
-  mirkill(trans);
-  mirkill(trans1);
-  mirkill(r);
+  // void mirkill* (big x)
+  // Securely kills off a big/flash number by zeroising it, and freeing its
+  // memory.
+  mirkill(a);
+  mirkill(pminus2);
+  mirkill(pminus1);
+  mirkill(rval);
   mirkill(mgcd);
+  mirkill(constnum1);
+  mirkill(constnum2);
 }
