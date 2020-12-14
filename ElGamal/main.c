635ba9c3ab035b165c84df259642d6254fe89486
markyangcc <guangshengyang01@gmail.com>
2020-12-14 18:18:37 +0800

    style: print style

--- ElGamal/main.c
+++ ElGamal/main.c
@@ -1,195 +1,130 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <time.h>
 
 #include "include/miracl.h"
+#define MAX_D 150  //安全素数 p 的位数
 
-void encrypt(big m, big p, big g, big y, big cs[2]);
-big decrypt(big cs[2], big p, big x);
-
-int main(int argc, char* argv[]) {
-  miracl* mip = mirsys(1000, 10);  //初始化大数系统为1000位的十进制数
-  mip->IOBASE = 10;                //指定10进制
-
-  char buf[1000] = {0};
-  char msg[150] = {0};
-
-  FILE* fp = NULL;
-  big two, one, p, q, g, x, y, m, c, tmp, psubtwo;
-  big cs[2], ret;
-
-  //读取文件数据
-  if (argc != 2) {
-    fprintf(stderr, "usage: %s <message-file-path>\n", argv[0]);
-    mirexit();
+int main() {
+  //-------------------------------------------------------------
+  //验证所需文件是否存在
+  FILE *fp;
+  char fpname[255];
+  printf("输入文件名称(包括路径名): ");  //明文文件的路径
+  scanf("%s", fpname);
+  if ((fp = fopen(fpname, "r")) == NULL) {
+    fprintf(stderr, "Error,can't open file \"%s\"\n", fpname);
     exit(EXIT_FAILURE);
   }
-  if ((fp = fopen(argv[1], "r")) == NULL) {
-    fprintf(stderr, "file open err: can't open file %s !\n", argv[1]);
-    mirexit();
-    exit(EXIT_FAILURE);
-  }
-  if (fread(msg, sizeof(char), 150, fp) <= 0) {
-    fprintf(stderr, "file read err: can't read file %s !\n", argv[1]);
-    fclose(fp);
-    mirexit();
-    exit(EXIT_FAILURE);
-  }
-  fclose(fp);
 
-  //初始化大数
-  one = mirvar(1);
-  two = mirvar(2);
-  p = mirvar(0);
-  q = mirvar(0);
-  g = mirvar(2);
-  x = mirvar(0);
-  y = mirvar(0);
-  m = mirvar(0);
-  c = mirvar(0);
-  tmp = mirvar(0);
-  psubtwo = mirvar(0);
-  irand(time(NULL));
+  //-------------------------------------------------------------
+  //密钥生成
+  miracl *mip = mirsys(1000, 10);  //初始化一个1000个10进制数的大数系统
+  mip->IOBASE = 10;                //指定IO使用的是10进制
 
-  //
-  cinstr(m, msg);
+  big p = mirvar(0);
+  big p_1 = mirvar(0);  // p-1
+  big p_2 = mirvar(0);  // p-2
+  big q = mirvar(0);
+  big g = mirvar(0);
+  big x = mirvar(0);
+  big y = mirvar(0);
+  big y1 = mirvar(0), y2 = mirvar(0);
+  big m = mirvar(0);   //明文
+  big m1 = mirvar(0);  //解密出的明文
+  big k = mirvar(0);
+  big flag = mirvar(0);       //中间变量
+  big constnum1 = mirvar(1);  //常量1
 
-  //选取随机素数q和p
+  irand((unsigned)time(NULL));  // 使用当前时间作为随机数种子
+                                //随机生成一个安全素数p
+  bigdig(MAX_D, 10, q);         //生成一个150位的随机数
+  nxsafeprime(0, 0, q, p);      //生成一个比q大的安全素数p
+  copy(p, q);
+  decr(q, 1, q);
+  subdiv(q, 2, q);  //生成q=(p-1)/2
+  decr(p, 1, p_1);  //生成p_1=p-1
+  decr(p, 2, p_2);  //生成p_2=p-2
+  //寻找一个本原根
+  // irand((unsigned)time(NULL)); // 使用当前时间作为随机数种子
   while (1) {
-    bigbits(499, q);
-    if (isprime(q)) break;
-  }
-
-  while (1) {
-    add(q, two, q);
-    if (!isprime(q)) {
+    bigrand(p_1, g);                    // g小于p-1
+    if (mr_compare(g, constnum1) <= 0)  //保证g大于1
       continue;
-    }
-    add(q, q, p);
-    add(p, one, p);
-    if (isprime(p)) {
-      break;
-    }
-  }
-
-  /*do {
-          bigbits(500, tmp);
-  } while (!nxsafeprime(0, 0, tmp, p));
-  decr(p, 1, q);
-  sftbit(q, 1, q);*/
-
-  //获取生成元g
-  do {
-    powmod(g, two, p, tmp);
-    if (mr_compare(tmp, one) > 0) {
-      powmod(g, q, p, tmp);
-      if (mr_compare(tmp, one) > 0) {
-        break;
+    powmod(g, mirvar(2), p, flag);
+    if (mr_compare(flag, constnum1) != 0) {
+      powmod(g, q, p, flag);
+      if (mr_compare(flag, constnum1) != 0) {
+        multiply(q, mirvar(2), flag);
+        powmod(g, flag, p, flag);
+        if (mr_compare(flag, constnum1) == 0) break;
       }
     }
-    add(g, one, g);
+  }  // end
 
-  } while (mr_compare(g, p) < 0);
-
-  //随机取x
-  decr(p, 2, psubtwo);
-  do {
-    bigrand(psubtwo, x);
-  } while (mr_compare(x, one) <= 0);
-
+  //随机选择私钥x，1<x<p-2
+  irand((unsigned)time(NULL));  // 使用当前时间作为随机数种子
+  while (1) {
+    bigrand(p_2, x);                   // x <p-2
+    if (mr_compare(x, constnum1) > 0)  //保证x>1
+      break;
+  }  // end
   //计算y
   powmod(g, x, p, y);
+  // end
+  printf("\n-------公布公钥（p，g，y）--------\n");
+  printf("p = \n");
+  cotnum(p, stdout);
+  printf("g = \n");
+  cotnum(g, stdout);
+  printf("y = \n");
+  cotnum(y, stdout);
+  printf("\n--------私钥----------------------\n");
+  cotnum(x, stdout);
 
-  //输出公钥
-  printf("公钥：\n");
-  memset(buf, 0, 501);
-  cotstr(p, buf);
-  printf("p = %s\n", buf);
-  memset(buf, 0, 501);
-  cotstr(g, buf);
-  printf("g = %s\n", buf);
-  memset(buf, 0, 501);
-  cotstr(y, buf);
-  printf("y = %s\n", buf);
+  //-------------------------------------------------------------
+  //加密算法
+
+  //随机生成k，使得1<=k<=p-2,(k,p-1)=1
+  irand((unsigned)time(NULL));  // 随机数种子
+  while (1) {
+    bigrand(p_1, k);                   // k小于p-1
+    if (mr_compare(k, constnum1) < 0)  //保证k>=1
+      continue;
+    egcd(k, p_1, flag);
+    if (mr_compare(flag, constnum1) == 0) break;
+  }  // end
+  //计算y1=g^k mod p
+  powmod(g, k, p, y1);
+  //计算y2=my^k mod p
+  cinnum(m, fp);
+  powmod2(m, constnum1, y, k, p, y2);
   printf("\n");
+  printf("------------密文（y1，y2）-----------\n");
+  printf("y1 = \n");
+  cotnum(y1, stdout);
+  printf("y2 = \n");
+  cotnum(y2, stdout);
 
-  //输出私钥
-  printf("私钥：\n");
-  memset(buf, 0, 501);
-  cotstr(x, buf);
-  printf("x = %s\n", buf);
-  printf("\n");
+  //-------------------------------------------------------------
+  //解密算法
 
-  //加密
-  encrypt(m, p, g, y, cs);
-
-  //输出y1和y2
-  printf("密文：\n");
-  memset(buf, 0, 501);
-  cotstr(cs[0], buf);
-  printf("y1 = %s\n", buf);
-  memset(buf, 0, 501);
-  cotstr(cs[1], buf);
-  printf("y2 = %s\n", buf);
-  printf("\n");
-
-  //解密
-  ret = decrypt(cs, p, x);
-
-  //输出还原的消息
-  printf("明文：\n");
-  memset(buf, 0, 501);
-  cotstr(ret, buf);
-  printf("message = %s\n", buf);
-  printf("\n");
+  //计算m1
+  copy(y1, flag);
+  xgcd(flag, p, flag, flag, flag);  //求y1的逆放到flag里面
+  powmod(flag, x, p, y1);           //求(y1的逆)的x次方  mod p
+  powmod(y2, constnum1, p, y2);     // y2 mod p
+  powmod2(y1, constnum1, y2, constnum1, p, m1);
+  printf("\n------------解密明文-----------------\n");
+  cotnum(m1, stdout);
+  if (mr_compare(m, m1) == 0) {
+    printf("\n");
+    printf("成功\n");
+  } else
+    printf("false\n");
 
   mirexit();
+
   return 0;
-}
-
-void encrypt(big m, big p, big g, big y, big cs[2]) {
-  big one, psubone, k, tmp;
-  one = mirvar(1);
-  psubone = mirvar(0);
-  k = mirvar(0);
-  tmp = mirvar(0);
-  cs[0] = mirvar(0);
-  cs[1] = mirvar(0);
-
-  decr(p, 1, psubone);
-  while (1) {
-    bigrand(psubone, k);
-    if (mr_compare(k, one) >= 0) {
-      egcd(k, y, tmp);
-      if (mr_compare(tmp, one) == 0) break;
-    }
-  }
-  powmod(g, k, p, cs[0]);
-  powmod(y, k, p, cs[1]);
-  multiply(cs[1], m, cs[1]);
-  powmod(cs[1], one, p, cs[1]);
-
-  mirkill(one);
-  mirkill(psubone);
-  mirkill(k);
-  return;
-}
-
-big decrypt(big cs[2], big p, big x) {
-  big tmp, one, ret;
-
-  tmp = mirvar(0);
-  ret = mirvar(0);
-  one = mirvar(1);
-
-  powmod(cs[0], x, p, tmp);
-  xgcd(tmp, p, tmp, tmp, tmp);
-  fft_mult(cs[1], tmp, tmp);
-  powmod(tmp, one, p, ret);
-
-  mirkill(tmp);
-  mirkill(one);
-
-  return ret;
 }
