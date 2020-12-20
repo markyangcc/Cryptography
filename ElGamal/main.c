a4dea729be178b8ef4ec45c9afe150579d4a0352
markyangcc <guangshengyang01@gmail.com>
2020-12-21 00:51:00 +0800

    refactor: update method( looking primitive root)

--- ElGamal/main.c
+++ ElGamal/main.c
@@ -23,25 +23,25 @@ int main() {
   //密钥生成
   //公钥 (p,g,g^a)
   //私钥 a
+  //
   miracl *mip = mirsys(1000, 10);  //初始化一个1000个10进制数的大数系统
   mip->IOBASE = 10;                //指定IO使用的是10进制
 
-  big p = mirvar(0);
-  big g = mirvar(0);
+  big p = mirvar(0);        //随机大素数
+  big g = mirvar(0);        //原根
   big pminus1 = mirvar(0);  // p-1
   big pminus2 = mirvar(0);  // p-2
-  big q = mirvar(0);
-  big a = mirvar(0);  //密钥
-  big y = mirvar(0);
+  big q = mirvar(0);        //中间变量
+  big a = mirvar(0);        //密钥
+  big y = mirvar(0);        // g^a
   big C1 = mirvar(0), C2 = mirvar(0);
-  big m = mirvar(0);   //明文
-  big m1 = mirvar(0);  //解密出的明文
-  big k = mirvar(0);
-  big flag = mirvar(0);       //中间变量
+  big m = mirvar(0);          //读取的明文
+  big m1 = mirvar(0);         //解密出的明文
+  big k = mirvar(0);          //随机数
+  big temp = mirvar(0);       //中间变量
   big constnum1 = mirvar(1);  //常量1
 
   irand((unsigned)time(NULL));  // 使用当前时间作为随机数种子
-                                //随机生成一个安全素数p
 
   // void bigdig (int n, int b, big x)
   // 生成一个给定长度的 b进制的大数并赋值给 x
@@ -50,6 +50,9 @@ int main() {
   // BOOL nxsafeprime (int type, int subset, big w, big p)
   // type: 素数的类型 q = (p − 1)/2 (type=0) or q = (p + 1)/2 (type=1)
   nxsafeprime(0, 0, q, p);  //生成下一个比q大的安全素数p
+
+  decr(p, 1, pminus1);  //大数减法，生成pminus1 = p-1
+  decr(p, 2, pminus2);  //大数减法，生成pminus2 = p-2
 
   // void copy* (flash x, flash y)
   // y = x
@@ -63,46 +66,33 @@ int main() {
   // z = x/n
   subdiv(q, 2, q);  //大数除法，q = (p-1)/2
 
-  decr(p, 1, pminus1);  //大数减法，生成pminus1 = p-1
-  decr(p, 2, pminus2);  //大数减法，生成pminus2 = p-2
-
   //==================================================================
   //选择随机大素数p的一个原根
   //
   //若g满足 g^2 mod p !=1 且 g^q mod p !=1，则说明g是一个原根
-  //
-  //设p为安全素数，即使p=2q+1，且q为素数。根据Fermat定理，
-  //如果g是原根，则满足这个公式g^(p-1) mod p =1
-  // 即g^(2q) mod p=1，因而如果
-  // w=min{t>0: g^t mod p=1}
-  // 则有w整除p-1=2q，因而由q是素数知，w只能是2或q，
-  while (1) {
+  while (true) {
     // void bigrand (big w, big x)
     //随机生成一个介于[0,w)之间的随机大数
     // 0 <= x < w
-    bigrand(pminus1, g);  //随机生成一个特定范围的原根, g小于p-1，大于0
+    bigrand(pminus1, g);  //随机生成一个特定范围的原根, g小于p-1
 
     // int compare* (big x, big y)
     // +1 if x > y; 0 if x = y; -1 if x < y
     if (mr_compare(g, constnum1) <= 0)  //保证生成的原根g是大于1的
-      continue;  //如果生成的g等于或者小于1都继续循环生成
+      continue;
+
+    big tmp1 = mirvar(0);
+    big tmp2 = mirvar(0);
 
     // void powmod (big x, big y, big n, big w)
     // w = x^y (mod n)
-    powmod(g, mirvar(2), p, flag);  // 构造flag = g^2 (mod p)
-    //如果flag不等于1，即g与2同余
-    if (mr_compare(flag, constnum1) != 0) {
-      powmod(g, q, p, flag);  // 构造flag = g^q (mod p)
-      //如果flag不等于1,即 g与2同余且与q同余，此时说明g是原根
-      if (mr_compare(flag, constnum1) != 0) {
-        // void multiply (big x, big y, big z)
-        // z = xy
-        multiply(q, mirvar(2), flag);  // flag = q*2
-        powmod(g, flag, p, flag);      // flag = g^flag (mod p)，
-
-        //如果flag =1 退出循环，即 g^2q (mod p) = 1 时退出循环
-        if (mr_compare(flag, constnum1) == 0) break;
-      }
+    powmod(g, mirvar(2), p, tmp1);  // 构造 tmp1 = g^2 (mod p)
+    powmod(g, q, p, tmp2);          // 构造 tmp2 = g^q (mod p)
+    //如果 tmp1 不等于1，即 g 与2同余 如果 tmp2 不等于1 即 g与 q同余
+    if (mr_compare(tmp1, constnum1) != 0 && mr_compare(tmp2, constnum1) != 0) {
+      powmod(g, pminus1, p, temp);  // temp = g^(p-1) mod p
+      if (mr_compare(temp, constnum1) == 0)
+        break;  //如果temp =1 说明g是原根，跳出循环即可
     }
   }
 
@@ -166,15 +156,15 @@ int main() {
   //计算m1
   // void copy* (flash x, flash y)
   // y = x
-  copy(C1, flag);  // flag = C1
+  copy(C1, temp);  // temp = C1
 
   // int xgcd (big x, big y, big xd, big yd, big z)
   // z = gcd(x, y) = (x * xd) + (y * yd)
   // 常用举例（求逆）：xgcd(x, p, x, x, x,); // x = 1/x mod p (p is prime)
-  xgcd(flag, p, flag, flag, flag);  // 代换后即flag是C1的逆，flag = 1/C1 mod p
+  xgcd(temp, p, temp, temp, temp);  // 代换后即temp是C1的逆，temp = 1/C1 mod p
 
   big V_r = mirvar(0);
-  powmod(flag, a, p, V_r);  // V_r = （C^(-1)）^a mod p = V^(-1)
+  powmod(temp, a, p, V_r);  // V_r = （C^(-1)）^a mod p = V^(-1)
 
   powmod(C2, constnum1, p, C2);  // C2 = C2 mod p
 
